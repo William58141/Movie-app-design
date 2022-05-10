d84a1faf802a9c9f1e506c192cf61ed3804dc9d5
William58141 <willam.fosmark@outlook.com>
2022-05-10 23:51:03 +0200

    Add project files.

---
+++ WpfApp1/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Core;
+
+namespace WpfApp1.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand HomeViewCommand { get; set; }
+
+        public RelayCommand DiscoveryViewCommand { get; set; }
+
+        public HomeViewModel HomeVm { get; set; }
+
+        public DiscoveryViewModel DiscoveryVm { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVm = new HomeViewModel();
+            DiscoveryVm = new DiscoveryViewModel();
+            CurrentView = HomeVm;
+
+            HomeViewCommand = new RelayCommand(o => 
+            {
+                CurrentView = HomeVm;
+            });
+
+            DiscoveryViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = DiscoveryVm;
+            });
+        }
+    }
+}
